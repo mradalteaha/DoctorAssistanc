13466019df84e2384d8ddab91a61eba47d158fde
moradteaha <73730311+moradteaha@users.noreply.github.com>
2021-06-13 11:44:52 +0300

    final ed

--- apput.py
+++ apput.py
@@ -1,5 +1,6 @@
 import unittest
 import pyrebase
+
 import os
 import sys
 import time
@@ -9,8 +10,9 @@ from kivy.clock import Clock
 from kivy.tests.common import GraphicUnitTest
 from kivy.uix.screenmanager import Screen
 from kivy.uix.button import Button
+
 firebaseConfig = {
-    'apiKey': "AIzaSyCVxh3zfkNPlbPTaUmhHgIRISlWl6l4qBY",
+             'apiKey': "AIzaSyCVxh3zfkNPlbPTaUmhHgIRISlWl6l4qBY",
     'authDomain': "doctorassistance-bce5c.firebaseapp.com",
     'databaseURL': "https://doctorassistance-bce5c-default-rtdb.firebaseio.com",
     'projectId': "doctorassistance-bce5c",
@@ -22,34 +24,60 @@ firebase = pyrebase.initialize_app(firebaseConfig)
 db = firebase.database()
 auth = firebase.auth()
 
-
 class TestUser(unittest.TestCase):
     def test_userid(self):
         '''this test to check if  user id found in the data base'''
         from main import find_user
-        self.assertEqual(1,find_user('123321123'),"User in the data base")
+        self.assertEqual(1,find_user('209102250'),"User in the data base")
         self.assertEqual(0, find_user('1111111'),"User is not in the data base")
 
     def test_username(self):
         '''this test to check if  username exist  in the data base'''
         from main import username_exist
-        self.assertEqual(1,username_exist('morad98'),"user name exists in data base")
+        self.assertEqual(1,username_exist('abdal98'),"user name exists in data base")
         self.assertEqual(0, username_exist('ZZZZZ'),"user name doesnt exist in data base")
 
-    def test_granaccess(self):#shayef hne
+    def test_granaccess(self):
         '''this test to check if  username and the password are correct and grant user acces'''
         from main import grantAccess
-        self.assertEqual(1,grantAccess('morad98','m@123456','123321123'),"Grant access")
-        self.assertEqual(0, grantAccess('morad98', '6548','123321123'), "Wrong password")
+        self.assertEqual(1,grantAccess('abdal98','abdal1998@','209102250'),"Grant access")
+        self.assertEqual(0, grantAccess('abd10', '6548','050'), "Wrong password")
 
-    def test_password_check(self):
-        '''this test to check if password is strong enough'''
-        from main import password_check
-        self.assertEqual(0,password_check('12345'),"password isn't long enough")
-        self.assertEqual(0,password_check('1234547'),"password should have a character")
-        self.assertEqual(0, password_check('1234547m'), "password should have a special character")
-        self.assertEqual(1, password_check('123454@7m'), "good password")
+    def test_currentuser(self):
+        '''this test return the id of the current users'''
+        from main import currentUser
+        self.assertEqual('209102250',currentUser('abdal98'),"returned the username ")
+        self.assertNotEqual('123456',currentUser('abd10'),"not the username of this id")
 
+
+from kivy.tests.common import GraphicUnitTest, UnitTestTouch
+from kivy.lang import Builder
+from kivy.uix.textinput import TextInput
+
+
+class TestGraphic(GraphicUnitTest):
+    '''This GUI test create a button and test the press function'''
+    def test_testButton(self):
+        def test_button(self):
+            from kivy.uix.widget import Widget
+            from kivy.uix.button import Button
+            from kivy.graphics import  Color
+
+            button = Button()
+            r = self.render
+            # create a root widget
+            wid = Widget()
+            with wid.canvas:
+                Color(1, 1, 1, 1)
+            wid.add_widget(button)
+            button.bind(
+                on_release=lambda instance: setattr(
+                    instance, 'test_released', True
+                )
+            )
+            self.assertTrue(button.test_released)
+
+            '''r(wid)'''
 
 if __name__ == '__main__':
     unittest.main()
